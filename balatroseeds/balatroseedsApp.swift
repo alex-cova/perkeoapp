9e2a73b7b5f81c0f359630375f9ad4716d15edfe
Alex <invokevirtual@icloud.com>
2025-01-03 14:28:16 -0600

    Initial Commit

---
+++ balatroseeds/balatroseedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  balatroseedsApp.swift
+//  balatroseeds
+//
+//  Created by Alex on 03/01/25.
+//
+
+import SwiftUI
+
+@main
+struct balatroseedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ balatroseedsTests/balatroseedsTests.swift
@@ -0,0 +1,17 @@
+//
+//  balatroseedsTests.swift
+//  balatroseedsTests
+//
+//  Created by Alex on 03/01/25.
+//
+
+import Testing
+@testable import balatroseeds
+
+struct balatroseedsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
