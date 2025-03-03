88a488b5505bb2b17b876219f0189f4cf89693b4
Alex <invokevirtual@icloud.com>
2025-03-02 20:52:58 -0600

    lock bug fix

--- balatroseeds/Lock.swift
+++ balatroseeds/Lock.swift
@@ -5,24 +5,24 @@
 //  Created by Alex on 03/01/25.
 //
 class Lock {
-
+    
     private var lock: [Int] = Array(repeating: 0, count: 13)
-
+    
     func unlock(_ item: Item) {
         lock[item.y] = lock[item.y] & ~(1 << item.ordinal)
     }
-
+    
     func lock(_ item: Item) {
         lock[item.y] = lock[item.y] | (1 << item.ordinal)
     }
-
+    
     func isLocked(_ item: Item) -> Bool {
         return (lock[item.y] & (1 << item.ordinal)) != 0
     }
-
+    
     func initUnlocks(_ ante: Int, _ freshProfile: Bool) {
         if ante <= 1 || ante > 6 { return }
-
+        
         if ante == 2 {
             unlock(Boss.The_Mouth)
             unlock(Boss.The_Fish)
@@ -34,7 +34,7 @@ class Lock {
             unlock(Boss.The_Water)
             unlock(Boss.The_Needle)
             unlock(Boss.The_Flint)
-
+            
             unlock(Tag.Standard_Tag)
             unlock(Tag.Meteor_Tag)
             unlock(Tag.Buffoon_Tag)
@@ -43,7 +43,7 @@ class Lock {
             unlock(Tag.Ethereal_Tag)
             unlock(Tag.Top_up_Tag)
             unlock(Tag.Orbital_Tag)
-
+            
             if !freshProfile { unlock(Tag.Negative_Tag) }
         }
         if ante == 3 {
@@ -54,23 +54,45 @@ class Lock {
         if ante == 5 { unlock(Boss.The_Serpent) }
         if ante == 6 { unlock(Boss.The_Ox) }
     }
-
+    
     func initLocks(_ ante: Int, _ freshProfile: Bool, _ freshRun: Bool) {
+        if (ante < 2) {
+            lock(Boss.The_Mouth);
+            lock(Boss.The_Fish);
+            lock(Boss.The_Wall);
+            lock(Boss.The_House);
+            lock(Boss.The_Mark);
+            lock(Boss.The_Wheel);
+            lock(Boss.The_Arm);
+            lock(Boss.The_Water);
+            lock(Boss.The_Needle);
+            lock(Boss.The_Flint);
+            lock(Tag.Negative_Tag);
+            lock(Tag.Standard_Tag);
+            lock(Tag.Meteor_Tag);
+            lock(Tag.Buffoon_Tag);
+            lock(Tag.Handy_Tag);
+            lock(Tag.Garbage_Tag);
+            lock(Tag.Ethereal_Tag);
+            lock(Tag.Top_up_Tag);
+            lock(Tag.Orbital_Tag);
+        }
+        
         if ante < 3 {
             lock(Boss.The_Tooth)
             lock(Boss.The_Eye)
         }
-
+        
         if ante < 4 { lock(Boss.The_Plant) }
         if ante < 5 { lock(Boss.The_Serpent) }
         if ante < 6 { lock(Boss.The_Ox) }
-
+        
         if freshProfile {
             lock(CommonJoker.Golden_Ticket)
             lock(CommonJoker.Hanging_Chad)
             lock(CommonJoker.Shoot_the_Moo)
             lock(CommonJoker.Swashbuckler)
-
+            
             lock(RareJoker.Blueprint)
             lock(RareJoker.Brainstorm)
             lock(RareJoker.Drivers_License)
@@ -82,12 +104,12 @@ class Lock {
             lock(RareJoker.The_Tribe)
             lock(RareJoker.The_Trio)
             lock(RareJoker.Wee_Joker)
-
+            
             lock(Tag.Foil_Tag)
             lock(Tag.Holographic_Tag)
             lock(Tag.Polychrome_Tag)
             lock(Tag.Rare_Tag)
-
+            
             lock(UnCommonJoker.Acrobat)
             lock(UnCommonJoker.Arrowhead)
             lock(UnCommonJoker.Bloodstone)
@@ -113,7 +135,7 @@ class Lock {
             lock(UnCommonJoker100.Stuntman)
             lock(UnCommonJoker100.The_Idol)
             lock(UnCommonJoker101C.Oops_All_6s)
-
+            
             lock(Voucher.Antimatter)
             lock(Voucher.Glow_Up)
             lock(Voucher.Illusion)
@@ -130,10 +152,10 @@ class Lock {
             lock(Voucher.Reroll_Glut)
             lock(Voucher.Retcon)
             lock(Voucher.Tarot_Tycoon)
-
+            
             lock(Tag.Negative_Tag)
         }
-
+        
         if freshRun {
             lock(Planet.Planet_X)
             lock(Planet.Ceres)
@@ -145,10 +167,10 @@ class Lock {
             lock(UnCommonJoker.Steel_Joker)
             lock(UnCommonJoker.Glass_Joker)
             lock(UnCommonJoker100.Lucky_Cat)
-
+            
             lock(CommonJoker.Golden_Ticket)
             lock(CommonJoker.Cavendish)
-
+            
             lock(Voucher.Overstock_Plus)
             lock(Voucher.Liquidation)
             lock(Voucher.Glow_Up)
@@ -167,7 +189,7 @@ class Lock {
             lock(Voucher.Palett)
         }
     }
-
+    
     func firstLock() {
         lock(Voucher.Overstock_Plus)
         lock(Voucher.Liquidation)
@@ -186,6 +208,6 @@ class Lock {
         lock(Voucher.Retcon)
         lock(Voucher.Palett)
     }
-
+    
 }
 
